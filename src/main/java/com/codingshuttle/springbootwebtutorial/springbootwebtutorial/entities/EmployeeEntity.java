aa33edeba94078597ced91d8477e0b497652e74d
Abhijit Kumar <kumarabhijeet563@gmail.com>
2026-09-21 22:12:46 +0530

    Chapter 2.3

---
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/entities/EmployeeEntity.java
@@ -0,0 +1,29 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+}
